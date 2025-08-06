9ee2d0aaee0c01be6b94c7fe60a73c6532f0dba9
drazicmartin <41931010+drazicmartin@users.noreply.github.com>
2025-08-06 11:04:01 +0000

    feat: add default config for IDEMIA

--- core/config/default.ts
+++ core/config/default.ts
@@ -21,17 +21,48 @@ export const defaultContextProvidersJetBrains: NonNullable<
 ];
 
 export const defaultConfig: ConfigYaml = {
-  name: "Local Assistant",
+  name: "IDEMIA Assistant",
   version: "1.0.0",
-  schema: "v1",
-  models: [],
+  schema: "v2",
+  models: [
+    {
+      name: "Idemia DeepLLM Assistant",
+      provider: "ollama",
+      model: "AUTODETECT",
+      apiBase: "http://deep26:3001",
+      roles: ["chat", "edit", "apply", "autocomplete"],
+      defaultCompletionOptions: { contextLength: 40960 },
+    },
+  ],
+  prompts: [
+    {
+      name: "Check",
+      description: "Check for errors in the code",
+      prompt: `Please read the highlighted code and check for any mistakes. You should look for the following, and be extremely vigilant:
+        - Syntax errors
+        - Logic errors
+        - Security vulnerabilities
+        - Performance issues
+        - Anything else that looks wrong
+        Once you find an error, please explain it as clearly as possible, but without using extra words. For example, instead of saying 'I think there is a syntax error on line 5', you should say 'Syntax error on line 5'. Give your answer as one bullet point per mistake found.`, 
+    }
+  ],
   context: defaultContextProvidersVsCode,
 };
 
 export const defaultConfigJetBrains: ConfigYaml = {
   name: "Local Assistant",
   version: "1.0.0",
-  schema: "v1",
-  models: [],
+  schema: "v2",
+  models: [
+    {
+      name: "Idemia DeepLLM Assistant",
+      provider: "ollama",
+      model: "AUTODETECT",
+      apiBase: "http://deep26:3001",
+      roles: ["chat", "edit", "apply", "autocomplete"],
+      defaultCompletionOptions: { contextLength: 40960 },
+    },
+  ],
   context: defaultContextProvidersJetBrains,
 };
